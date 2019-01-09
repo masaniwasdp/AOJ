7a0ccdfcc53284587604ee3e4fd93a161de5a6fc
masaniwa <masaniwasdp@gmail.com>
2019-01-10 00:51:26 +0900

    Modifis iomete.

--- Volume0/Vol0050.c
+++ Volume0/Vol0050.c
@@ -9,88 +9,29 @@ int main(void)
 
     while (str[i] && i < 996)
     {
-        if (str[i + 0] == 'a')
+        if (str[i] == 'a' && str[i + 1] == 'p' && str[i + 2] == 'p' && str[i + 3] == 'l' && str[i + 4] == 'e')
         {
-            if (str[i + 1] == 'p')
-            {
-                if (str[i + 2] == 'p')
-                {
-                    if (str[i + 3] == 'l')
-                    {
-                        if (str[i + 4] == 'e')
-                        {
-                            str[i + 0] = 'p';
-                            str[i + 1] = 'e';
-                            str[i + 2] = 'a';
-                            str[i + 3] = 'c';
-                            str[i + 4] = 'h';
-                             
-                            i += 5;
-                        }
-                        else
-                        {
-                            i += 4;
-                        }
-                    }
-                    else
-                    {
-                        i += 2;
-                    }
-                }
-                else
-                {
-                    i++;
-                }
-            }
-            else
-            {
-                i++;
-            }
+            str[i + 0] = 'p';
+            str[i + 1] = 'e';
+            str[i + 2] = 'a';
+            str[i + 3] = 'c';
+            str[i + 4] = 'h';
+
+            i += 5;
+        }
+        else if (str[i] == 'p' && str[i + 1] == 'e' && str[i + 2] == 'a' && str[i + 3] == 'c' && str[i + 4] == 'h')
+        {
+            str[i + 0] = 'a';
+            str[i + 1] = 'p';
+            str[i + 2] = 'p';
+            str[i + 3] = 'l';
+            str[i + 4] = 'e';
+
+            i += 5;
         }
         else
         {
-            if (str[i + 0] == 'p')
-            {
-                if (str[i + 1] == 'e')
-                {
-                    if (str[i + 2] == 'a')
-                    {
-                        if (str[i + 3] == 'c')
-                        {
-                            if (str[i + 4] == 'h')
-                            {
-                                str[i + 0] = 'a';
-                                str[i + 1] = 'p';
-                                str[i + 2] = 'p';
-                                str[i + 3] = 'l';
-                                str[i + 4] = 'e';
-                                 
-                                i += 5;
-                            }
-                            else
-                            {
-                                i += 4;
-                            }
-                        }
-                        else
-                        {
-                            i += 2;
-                        }
-                    }
-                    else
-                    {
-                        i += 2;
-                    }
-                }
-                else
-                {
-                    i++;
-                }
-            }
-            else
-            {
-                i++;
-            }
+            i++;
         }
     }
 
