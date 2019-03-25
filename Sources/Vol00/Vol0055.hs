89a9a9d14f21eeef520c1676158db10e82bfb638
masaniwa <masaniwasdp@gmail.com>
2019-03-26 00:18:19 +0900

    Reskribis en Haskell.

---
+++ Sources/Vol00/Vol0055.hs
@@ -0,0 +1,36 @@
+import Control.Applicative
+import Data.List (intercalate)
+
+
+terms :: Int -> Double -> [Double]
+
+terms n a = eTerm (n - 1) [a]
+  where
+    oTerm 0 xs = reverse xs
+    oTerm m xs = eTerm (m - 1) $ (head xs / 3) : xs
+
+    eTerm 0 xs = reverse xs
+    eTerm m xs = oTerm (m - 1) $ (head xs * 2) : xs
+
+
+solve :: Double -> Double
+
+solve a = sum $ terms 10 a
+
+
+input :: String -> Double
+
+input = read
+
+
+output :: Double -> String
+
+output = show
+
+
+main :: IO ()
+
+main = do
+  xs <- lines <$> getContents
+
+  putStrLn . intercalate "\n" $ map (output . solve . input) xs
