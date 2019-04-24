dffb4e40552f73f0a52cdb0fe6bad9de7285cec8
masaniwa <masaniwasdp@gmail.com>
2019-04-25 02:35:37 +0900

    Reskribis en Haskell.

---
+++ Sources/Vol00/Vol0007.hs
@@ -0,0 +1,27 @@
+import Control.Applicative
+import Data.List (intercalate)
+
+
+solve :: Int -> Int
+
+solve n = (iterate next 100000) !! n
+  where
+    next = \x -> (* 1000) $ ceiling (1.05 * toRational x / 1000)
+
+
+input :: String -> Int
+
+input = read
+
+
+output :: Int -> String
+
+output = show
+
+
+main :: IO ()
+
+main = do
+  xs <- lines <$> getContents
+
+  putStrLn . intercalate "\n" $ map (output . solve . input) xs
