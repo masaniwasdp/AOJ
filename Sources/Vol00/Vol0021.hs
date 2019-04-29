c466bb4089097bc01442189e6f03b28df8099404
masaniwa <masaniwasdp@gmail.com>
2019-04-30 01:56:54 +0900

    Reskribis en Haskell.

---
+++ Sources/Vol00/Vol0021.hs
@@ -0,0 +1,34 @@
+import Control.Applicative
+import Data.List (intercalate)
+
+
+cross :: [Double] -> Double
+
+cross [x1, y1, x2, y2, x3, y3, x4, y4] = a
+  where
+    a = (x2 - x1) * (y4 - y3) - (x4 - x3) * (y2 - y1)
+
+cross _ = undefined
+
+
+solve :: [Double] -> Bool
+
+solve xs = abs (cross xs) < 1.0e-10
+
+
+input :: String -> [Double]
+
+input = map read . words
+
+
+output :: Bool -> String
+
+output x = if x then "YES" else "NO"
+
+
+main :: IO ()
+
+main = do
+  xs <- tail . lines <$> getContents
+
+  putStrLn . intercalate "\n" $ map (output . solve . input) xs
