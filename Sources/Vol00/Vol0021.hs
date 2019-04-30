97ad5d05b59a4bf51c53a694c6fffaf842558ecb
masaniwa <masaniwasdp@gmail.com>
2019-05-01 00:55:43 +0900

    Modifis stilon.

--- Sources/Vol00/Vol0021.hs
+++ Sources/Vol00/Vol0021.hs
@@ -4,9 +4,9 @@ import Data.List (intercalate)
 
 cross :: [Double] -> Double
 
-cross [x1, y1, x2, y2, x3, y3, x4, y4] = a
+cross [x1, y1, x2, y2, x3, y3, x4, y4] = rslt
   where
-    a = (x2 - x1) * (y4 - y3) - (x4 - x3) * (y2 - y1)
+    rslt = (x2 - x1) * (y4 - y3) - (x4 - x3) * (y2 - y1)
 
 cross _ = undefined
 
